674cd85ebde485fcd092fd94951ef6b4b07dbcb6
PlumberMario <52254876+MarioZZJ@users.noreply.github.com>
2020-07-25 16:36:48 +0800

    fix in FILTER
    取消对静态文件（/image、/css、/js、/fonts）下访问的记录，否则会产生大量记录，增加百度api并行压力和数据库访问压力

--- src/cn/mariozzj/sp2/filter/GlobalFilter.java
+++ src/cn/mariozzj/sp2/filter/GlobalFilter.java
@@ -19,20 +19,24 @@ public class GlobalFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(req, resp); //doFilter代码重复可能导致jsp页面内容重复
+//        chain.doFilter(req, resp); //doFilter代码重复可能导致jsp页面内容重复
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
         request.setCharacterEncoding("UTF-8");//在全局Filter中对中文进行适配
-        VisitRecord record = new VisitRecord();
-        record.ipaddr = request.getRemoteAddr();
-        record.location = (new Location()).GetLocation(record.ipaddr);
-        record.url = request.getRequestURL().toString();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date d = new Date();
-        record.date = sdf.format(d);
-        VisitRecordDAO vdao = new VisitRecordDAO();
-        vdao.add(record);
-        System.out.printf("%s %s 访问了 %s%n", record.date, record.ipaddr, record.url);
+        String url = String.valueOf(((HttpServletRequest) req).getRequestURL());
+        if(url.contains("/js") || url.contains("/image") || url.contains("/css") || url.contains("/fonts")) {}
+        else{
+            VisitRecord record = new VisitRecord();
+            record.ipaddr = request.getRemoteAddr();
+            record.location = (new Location()).GetLocation(record.ipaddr);
+            record.url = request.getRequestURL().toString();
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date d = new Date();
+            record.date = sdf.format(d);
+            VisitRecordDAO vdao = new VisitRecordDAO();
+            vdao.add(record);
+            System.out.printf("[P.M.I.C.U-INFO]%s %s has just requested %s%n", record.date, record.ipaddr, record.url);
+        }
         chain.doFilter(request, response);
     }
 
